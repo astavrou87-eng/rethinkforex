8a727fef2edc63abcfc9d6b913c075bbf7ea59b8
Andreas <astavrou87@gmail.com>
2026-02-22 02:14:52 +0000

    Force HTTPS and canonical domain redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // Force HTTPS if the request arrived as http at the Vercel edge
+      {
+        source: "/:path*",
+        has: [{ type: "header", key: "x-forwarded-proto", value: "http" }],
+        destination: "https://rethinkforex.co.uk/:path*",
+        permanent: true,
+      },
+
+      // Force canonical host (www -> apex)
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.rethinkforex.co.uk" }],
+        destination: "https://rethinkforex.co.uk/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
